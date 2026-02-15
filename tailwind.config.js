90fc30638c263bb9c236b78daf5992d4eb012e95
Krishnabhadauriya <krishnabhadauriya578@gmail.com>
2026-02-15 14:04:16 +0530

    Mission

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+   extend: {
+  animation: {
+    bounceSlow: "bounce 2s infinite",
+    zoomIn: "zoomIn 1s ease-out forwards",
+  },
+  keyframes: {
+    zoomIn: {
+      "0%": { transform: "scale(0.5)", opacity: "0" },
+      "100%": { transform: "scale(1)", opacity: "1" },
+    },
+  },
+}
   },
   plugins: [],
 }
